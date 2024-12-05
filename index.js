6419c58c17b1150ebfd2ae9996788f1c49584b5a
imran <sherlockholmesad221@gmail.com>
2024-12-05 19:19:35 +0600

    add get request for featured movies

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const dotenv = require("dotenv");
 dotenv.config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
 const PORT = process.env.PORT || 5000;
@@ -8,7 +8,6 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@mimha007.i7mwd.mongodb.net/?retryWrites=true&w=majority&appName=mimha007`;
 
@@ -18,7 +17,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -35,17 +34,27 @@ async function run() {
       res.send(result);
     });
 
-    app.post("/movies",async (req,res)=>{
+    app.get("/features", async (req, res) => {
+      const cursor = movieCollection
+        .find()
+        .sort({ movieRating: -1 })
+        .limit(6);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.post("/movies", async (req, res) => {
       const newMovie = req.body;
       console.log(newMovie);
       const result = await movieCollection.insertOne(newMovie);
       res.send(result);
-    })
-
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -53,12 +62,9 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
-
-app.get("/",(req,res)=>{
-    res.send(`server is running`);
-})
+app.get("/", (req, res) => {
+  res.send(`server is running`);
+});
 
 app.listen(PORT, () => {
   console.log(`server is running on the ${PORT}`);
